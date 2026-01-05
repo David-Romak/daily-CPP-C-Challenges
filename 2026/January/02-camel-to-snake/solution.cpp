2d36281651e734f37e16840d0f4e206e4a0ef15d
David-Romak <kizitodavid18@gmail.com>
2026-01-06 00:11:49 +0300

    CPP solution with string streams

--- 2026/January/02-camel-to-snake/solution.cpp
+++ 2026/January/02-camel-to-snake/solution.cpp
@@ -34,10 +34,28 @@ string camel_to_snake(const string &camel)
     return result;
 }
 
+string camel_to_snake_stream(const string& camel) {
+    if (camel.empty()) return "";
+    
+    stringstream ss;
+    ss << (char)tolower(camel[0]);
+    
+    for (size_t i = 1; i < camel.length(); i++) {
+        char c = camel[i];
+        if (isupper(c)) {
+            ss << '_' << (char)tolower(c);
+        } else {
+            ss << c;
+        }
+    }
+    
+    return ss.str();
+}
+
 int main()
 {
     cout << "---CamelCase to Snake_Case Converter - C++---\n";
-    string snake = camel_to_snake("camelCase");
+    string snake = camel_to_snake_stream("camelCase");
     cout << snake << endl;
 
     return 0;
